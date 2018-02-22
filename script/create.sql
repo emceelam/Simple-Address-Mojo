deebb9d982fe2e3932c4817ea2abe23c8c498d82
Lambert Lum <emceelam@gmail.com>
2018-02-21 22:14:30 -0800

    Delete an obsolete database table

--- script/create.sql
+++ script/create.sql
@@ -18,21 +18,3 @@ INSERT INTO addresses (street, city, state, zip)
 INSERT INTO addresses (street, city, state, zip)
   VALUES ("1077 E Arques Ave", "Sunnyvale", "CA", 94085);
   
-
-CREATE TABLE IF NOT EXISTS geocode (
-  id     INTEGER PRIMARY KEY ASC,
-  street TEXT,
-  city   TEXT,
-  state  TEXT,
-  zip    INTEGER,
-  lat    REAL,  /* latitude */
-  lng    REAL  /* longitude */
-);
-
-DELETE FROM geocode;
-
-INSERT INTO geocode (street, city, state, zip, lat, lng)
-  VALUES ("550 E Brokaw Rd", "San Jose", "CA", 95112, 37.3798781, -121.9068);
-
-INSERT INTO geocode (street, city, state, zip, lat, lng)
-  VALUES ("1077 E Arques Ave", "Sunnyvale", "CA", 94085, 37.3824601, -122.001514);
