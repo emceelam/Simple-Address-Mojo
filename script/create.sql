a5e5ad28c53744129744856940aae6d465606d27
Lambert Lum <emceelam@gmail.com>
2018-02-28 16:34:59 -0800

    Add more Frys Electronics locations to starter database.

--- script/create.sql
+++ script/create.sql
@@ -18,3 +18,11 @@ INSERT INTO addresses (street, city, state, zip)
 INSERT INTO addresses (street, city, state, zip)
   VALUES ("1077 E Arques Ave", "Sunnyvale", "CA", 94085);
   
+INSERT INTO addresses (street, city, state, zip)
+  VALUES ("43800 Osgood Rd", "Fremont", "CA", 94539);
+
+INSERT INTO addresses (street, city, state, zip)
+  VALUES ("600 E Hamilton Ave", "Campbell", "CA", 95008);
+
+INSERT INTO addresses (street, city, state, zip)
+  VALUES ("340 Portage Ave", "Palo Alto", "CA", 94306);
